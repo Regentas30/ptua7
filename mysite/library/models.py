9551a8b2de7ac8cfd934c3d3c44335927e8eb8b6
Paulius Skaisgiris <paulius.skaisgiris@gmail.com>
2023-11-09 16:47:04 +0100

    add material for django lesson 2: models

---
+++ mysite/library/models.py
@@ -0,0 +1,67 @@
+from django.db import models
+from django.urls import reverse
+import uuid
+
+
+class Genre(models.Model):
+    name = models.CharField('Pavadinimas', max_length=200, help_text='Knygos žanras')
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField('Pavadinimas', max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField('Aprašymas', max_length=1000,
+                               help_text='Knygos santrauka')
+    isbn = models.CharField('ISBN', max_length=13,
+                            help_text='13 simbolių <a href="https://www.isbn-international.org/content/what-isbn">ISBN kodas</a>')
+    genre = models.ManyToManyField(Genre, help_text='Pasirinkti žanrą')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('Autoriaus aprašymas', args=[str(self.id)])
+
+
+class BookInstance(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, help_text='Unikalus ID knygos kopijai')
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+    due_back = models.DateField('Bus prieinama', null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('a', 'Administruojama'),
+        ('p', 'Paimta'),
+        ('g', 'Galima paimti'),
+        ('r', 'Rezervuota')
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='a',
+        help_text='Statusas'
+    )
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        return f'{self.id} ({self.book.title})'
+
+
+class Author(models.Model):
+    first_name = models.CharField('Vardas', max_length=100)
+    last_name = models.CharField('Pavardė', max_length=100)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        return reverse('Autoriaus aprašymas', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name}'
---
+++ mysite/mysite/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('library/', include('library.urls')),
+    path('admin/', admin.site.urls),
+]
+
+
+
+
+
+
